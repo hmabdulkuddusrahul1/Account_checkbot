741ab0538a7ed6c704702a9345f187a736e8bae5
hmabdulkuddusrahul1 <108615434+hmabdulkuddusrahul1@users.noreply.github.com>
2022-08-14 13:20:17 +0600

    Update message.py

--- Bot/message.py
+++ Bot/message.py
@@ -8,7 +8,7 @@ logging.basicConfig(
 )
 logger = logging.getLogger(__name__)
 
-bot_token = os.environ.get('BOT_TOKEN')
+bot_token = os.environ.get('5465734618:AAEOmz4vr5jQTUW8fxfSlJElNKEifCKzJa4')
 bot = Bot(bot_token)
 
 def Sendmessage(chat_id, text,reply_markup=None):
